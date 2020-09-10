ef3618f147d556aef0b14e584d40d824b87c8d14
Yann Richet <yann.richet@laposte.net>
2020-09-10 18:08:45 +0200

    Add files via upload

---
+++ src/main/scripts/R.bat
@@ -0,0 +1,19 @@
+
+set PATH=%PATH%;C:\Program Files\R\R-3.6.1\bin
+start /b R.exe CMD BATCH %*
+for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq R.exe"') do set PID_R=%%b
+echo %PID_R% > PID
+
+:loop
+tasklist | find " %PID_R% " >nul
+if not errorlevel 1 (
+    timeout /t 1 >nul
+    goto :loop
+)
+
+del /f PID
+
+findstr "Err" *.Rout
+if  errorlevel 1 (
+    exit 1
+)
