a418e10eb15e81db8856bddb5644b3bd84e1f740
Yann Richet <richetyann@gmail.com>
2021-12-03 10:41:24 +0100

    Update R.bat

--- src/main/scripts/R.bat
+++ src/main/scripts/R.bat
@@ -1,18 +1,27 @@
+@echo off
 
-set PATH=%PATH%;C:\Program Files\R\R-3.6.1\bin
-start /b R.exe CMD BATCH %*
-for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq R.exe"') do set PID_R=%%b
+set PATH=%PATH%;C:\Program Files\R\R-4.1.0\bin
+
+set id=%RANDOM%
+start "R_%id%" cmd /c "R.exe CMD BATCH %*"
+set PID_R="none"
+for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "WINDOWTITLE eq R_%id%"') do set PID_R=%%b
 echo %PID_R% > PID
 
+if "%PID_R%" neq "none" (
 :loop
 tasklist | findstr " %PID_R% " >nul
 if not errorlevel 1 (
     timeout /t 1 >nul
     goto :loop
 )
+)
 
 del /f PID
 
+if not exist "*.Rout" (
+    exit 2
+)
 findstr "Err" *.Rout
 if not errorlevel 1 (
     exit 1
